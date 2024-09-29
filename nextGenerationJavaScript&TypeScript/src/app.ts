c6714d15911fbcb63bec6e55029adead0117b382
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-09-29 14:07:53 +0200

    let and const

---
+++ nextGenerationJavaScript&TypeScript/src/app.ts
@@ -0,0 +1,19 @@
+const userName = 'Max';
+
+let age = 30;
+
+age = 29;
+
+var result;
+
+function add(a: number, b: number) {
+    result = a + b;
+    return result;
+}
+
+if (age > 20) {
+    let isOld = true;
+    console.log(isOld)
+} 
+
+console.log()
