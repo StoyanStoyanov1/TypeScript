3b91e0273bf966784b8054bef87f457bcf7e6606
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-09-29 20:35:50 +0200

    creating a first class

---
+++ Classes/src/app.ts
@@ -0,0 +1,13 @@
+class Department {
+    name: string;
+
+    constructor(n: string) {
+        this.name = n;
+    }
+
+}
+
+const accounting = new Department('Stenly');
+
+console.log(accounting);
+
