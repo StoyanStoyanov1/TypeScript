25e1429197d1e1914428926e2818522c7b75162d
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-09-29 14:15:48 +0200

    arrow function and default parameters

--- nextGenerationJavaScript&TypeScript/src/app.ts
+++ nextGenerationJavaScript&TypeScript/src/app.ts
@@ -4,16 +4,26 @@ let age = 30;
 
 age = 29;
 
-var result;
+// var result;
 
-function add(a: number, b: number) {
-    result = a + b;
-    return result;
+// function add(a: number, b: number) {
+//     result = a + b;
+//     return result;
+// }
+
+// if (age > 20) {
+//     let isOld = true;
+//     console.log(isOld)
+// } 
+
+const add = (a: number, b: number = 1) => a + b;
+
+const printOutput: (a: number | string) => void = output => console.log(output);
+
+const button = document.querySelector('button');
+
+if (button) {
+    button.addEventListener('click', event => console.log(event));
 }
 
-if (age > 20) {
-    let isOld = true;
-    console.log(isOld)
-} 
-
-console.log()
+printOutput(add(5));
