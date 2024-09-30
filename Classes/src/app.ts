2e1fb1481f7a9d2c16ae62ee57a1c796f44fcd9d
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-09-30 13:54:26 +0200

    class functions

--- Classes/src/app.ts
+++ Classes/src/app.ts
@@ -1,13 +1,29 @@
 class Department {
     name: string;
+    employees: string[] = [];
 
     constructor(n: string) {
         this.name = n;
     }
 
+    describe(this: Department) {
+        console.log('Deparment: ' + this.name);
+    }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 
 const accounting = new Department('Stenly');
 
-console.log(accounting);
+accounting.describe();
 
+accounting.addEmployee('Other');
+accounting.addEmployee('Max');
+accounting.printEmployeeInformation();
