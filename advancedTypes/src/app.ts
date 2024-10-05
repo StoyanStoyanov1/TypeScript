26eb945119c70ea5cca5a30c649cf1fcadf16984
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-10-05 07:55:31 +0200

    more on type gurads

--- advancedTypes/src/app.ts
+++ advancedTypes/src/app.ts
@@ -15,3 +15,61 @@ const el: ElevatedEmployee = {
     privileges: ['create-server'],
     startDate: new Date(),
 }
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
+
+function add (a: Combinable, b: Combinable) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toLocaleString() + b.toLocaleString();
+    }
+    return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+    console.log('Name: ' + emp.name);
+    if ('privileges' in emp) {
+        console.log('Privileges: ' + emp.privileges)
+    }
+
+    if ('startDate' in emp) {
+        console.log('Start Date: ' + emp.startDate);
+    }
+}
+
+printEmployeeInformation({name: "Manu", startDate: new Date()});
+
+class Car {
+    drive() {
+        console.log('Driving...');
+    }
+}
+
+class Truck {
+    drive() {
+        console.log('Driving a truck...');
+    }
+
+    loadCargo(amount: number) {
+        console.log('Loading cargi ...' + amount);
+    }
+}
+
+type Vechicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vechicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+
+useVehicle(v1);
+useVehicle(v2);
