ec44d9c197d055a6f2d43fd2a69178da6fb15b4b
Stoyan Stoyanov <125189626+StoyanStoyanov1@users.noreply.github.com>
2025-01-15 20:34:10 +0100

    update

--- TypeScript/firstSteps/subtractionFunction/app.ts
+++
@@ -1,9 +0,0 @@
-function subtraction(n1: number, n2: number) {
-  return n1 - n2;
-}
-
-const number1 = 5;
-const number2 = 2.8;
-
-const result = subtraction(number1, number2);
-console.log(result);
