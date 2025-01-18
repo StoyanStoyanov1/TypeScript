17cc71481b126f4fd7e8166995189c4e78fc7a6f
Stoyan Stoyanov <125189626+StoyanStoyanov1@users.noreply.github.com>
2025-01-18 07:30:34 +0100

    Namespaces and Modules

---
+++ nameSpacesAndModulesExercises/src/app.ts
@@ -0,0 +1,8 @@
+import { MathUtils } from "./utils/math-utils";
+import { StringUtils } from "./utils/string-utils";
+
+let sum = MathUtils.add(5, 2);
+let combined = StringUtils.add("Hello ", "World");
+
+console.log(sum);
+console.log(combined);
