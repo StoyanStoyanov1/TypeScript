b1df4f3c83b4683ff7b4ce180e2423935c075ca6
Stoyan Stoyanov <125189626+StoyanStoyanov1@users.noreply.github.com>
2025-01-19 20:00:31 +0100

    conditional types

---
+++ advancedConceptsAndFeatures/src/conditionalTypes.ts
@@ -0,0 +1,15 @@
+interface Animal {
+    live: () => void;
+  }
+  
+  interface Dog extends Animal {
+    woof: () => void;
+  }
+  
+  type Example = Dog extends Animal ? string : number;
+  
+  // SomeType extends OtherType ? TrueType : FalseType;
+  type isString<T> = T extends string ? true : false;
+  
+  type A = isString<string>;
+  type B = isString<number>;
