b6e42de61a3f73e7f23fffa92209769ed320a537
Stoyan Stoyanov <125189626+StoyanStoyanov1@users.noreply.github.com>
2025-01-19 09:01:01 +0100

    declaration files

---
+++ declarationFIles/src/calculator.d.ts
@@ -0,0 +1,4 @@
+export declare function add(num1: number, num2: number): number;
+export declare function add(num1: string, num2: string): string;
+export declare function add(num1: number, num2: string): string;
+export declare function add(num1: string, num2: number): string;
