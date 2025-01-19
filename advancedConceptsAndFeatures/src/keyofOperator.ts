502fa9065f8a55b9d900a06274ba4d98f9f7d6bf
Stoyan Stoyanov <125189626+StoyanStoyanov1@users.noreply.github.com>
2025-01-19 19:26:31 +0100

    keyof operator

---
+++ advancedConceptsAndFeatures/src/keyofOperator.ts
@@ -0,0 +1,21 @@
+type Events = {
+    id: number;
+    date: Date;
+    type: 'indoor' | 'outdoor';
+  };
+  
+  // -> "id" | "date" | "type"
+  
+  type unionOfKeysOfEvents = keyof Events;
+  
+  type Numeric = {
+    [key: number]: string;
+  };
+  
+  type NumericKeyOf = keyof Numeric;
+  
+  type NumberAndString = {
+    [key: string]: string;
+  };
+  
+  type NumberAndStringKeyoff = keyof NumberAndString;
