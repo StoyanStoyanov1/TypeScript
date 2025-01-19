4ecb3df9c687e869bcb8fa85069ece448793c76b
Stoyan Stoyanov <125189626+StoyanStoyanov1@users.noreply.github.com>
2025-01-19 15:43:17 +0100

    type casting

---
+++ advancedConceptsAndFeatures/src/typeCasting.ts
@@ -0,0 +1,12 @@
+const numbers = {
+    x: 10,
+    y: {
+      z: 20,
+    },
+  } as const;
+  
+  let firstNameField = document.querySelector(
+    '#firstName'
+  )! as HTMLInputElement;
+  
+  firstNameField.value;
