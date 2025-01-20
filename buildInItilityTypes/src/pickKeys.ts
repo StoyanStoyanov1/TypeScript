bec86bffa8d0cab38dcc38f1ccd1588adec09a44
Stoyan Stoyanov <125189626+StoyanStoyanov1@users.noreply.github.com>
2025-01-20 10:51:26 +0100

    pick keys

---
+++ buildInItilityTypes/src/pickKeys.ts
@@ -0,0 +1,14 @@
+interface Person {
+    name: string;
+    age: number;
+    address: string;
+  }
+  
+  type NameAndAge = Pick<Person, "name" | "age">;
+  
+  const person: NameAndAge = {
+    name: "John",
+    age: 32,
+  };
+  
+  person.name = "Else";
