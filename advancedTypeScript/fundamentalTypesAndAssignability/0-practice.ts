05a947a398cc21ccef96b411342dac2765161647
StoyanStoyanov1 <stoyan.stoyanov93@yahoo.com>
2025-09-23 17:38:33 +0200

    update

---
+++ advancedTypeScript/fundamentalTypesAndAssignability/0-practice.ts
@@ -0,0 +1,8 @@
+export const emails: Set<String> = new Set();
+
+emails.add("test@gtest.com");
+emails.add("blabla@BlaBlaBla.org");
+
+// emails.add(true);
+// emails.add({ id: "jfsdlfjlksdjfdl" });
+console.log(emails);
