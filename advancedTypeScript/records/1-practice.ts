9b90ad10e7d37288580eff6727d47c7a2ca2c66c
StoyanStoyanov1 <stoyan.stoyanov93@yahoo.com>
2025-09-24 16:22:06 +0200

    feat: add inventory and regional forecasts, enhance blog post fetching, and implement settings merging

---
+++ advancedTypeScript/records/1-practice.ts
@@ -0,0 +1,29 @@
+type Region = "north" | "south" | "east" | "west";
+
+type RegionalForecasts = Record<string, 
+{temperature: number, precipitation: number}
+>;
+
+// Adding a property not in Region (e.g. "central") is not allowed.
+export const forecasts: RegionalForecasts = {
+  north: {
+    temperature: -5,
+    precipitation: 30,
+  },
+  south: {
+    temperature: 20,
+    precipitation: 10,
+  },
+  east: {
+    temperature: 15,
+    precipitation: 20,
+  },
+  west: {
+    temperature: 10,
+    precipitation: 40,
+  },
+  central: {
+    temperature: 12,
+    precipitation: 25,
+  },
+};
